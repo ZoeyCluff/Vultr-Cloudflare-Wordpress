3d675c8c7f64f7ff92678551f2ee84306d82954a
Scott King <Lvl4Sword@users.noreply.github.com>
2018-01-22 21:47:16 -0700

    Typos, open() -> with open(), .lower(), and
    raw_input is already a string.

--- Provision(Run Me).py
+++ Provision(Run Me).py
@@ -5,9 +5,9 @@ from twindb_cloudflare.twindb_cloudflare import CloudFlare, CloudFlareException
 import os
 from variables import *
 from vultr import *
-from cloudflareSearch import *
-from cloudflareDelete import *
-from cloudflareCheck import *
+from cloudflareSearch import zone, doRecordsExist, IPV4Record, IPV4wwwRecord, IPV6Record, IPV6wwwRecord, 
+from cloudflareDelete import ipv4RecordDelete, ipv4wwwRecordDelete, ipv6RecordDelete, ipv6wwwRecordDelete, deleteRecords
+from cloudflareCheck import ipv4, ipv4www, ipv6, ipv6www
 import sys
 import requests
 from blessings import Terminal
@@ -20,7 +20,7 @@ instanceID, ipv4, ipv6 = vultr(apikey)
 
 # Doing some Cloudflare checks before continuining.
 
-domain = str(raw_input("What is the domain name? (without www): "))
+domain = raw_input("What is the domain name? (without www): ")
 domainLong = str('www.' + domain)
 
 zone = zone(domain, CLOUDFLARE_EMAIL, CLOUDFLARE_AUTH_KEY)
@@ -28,32 +28,32 @@ zone = zone(domain, CLOUDFLARE_EMAIL, CLOUDFLARE_AUTH_KEY)
 exists = doRecordsExist(zone, CLOUDFLARE_EMAIL, CLOUDFLARE_AUTH_KEY)
 
 # print("Checking for existing A and AAAA records that may interfere")
-if ipv4Record:
+if IPV4Record:
     print term.yellow("Conflicting IPV4 @ record detected. Site will not function without updating this record. Update?")
     answerIPV4 = str(raw_input('Update ipv4 @ records? y/n (lowercase y or n): '))
 
-if ipv4wwwRecord:
+if IPV4wwwRecord:
     print term.yellow("Conflicting IPV4 www record detected. Site will not function without updating this record. Update?")
     answerIPV4WWW = str(raw_input('Update ipv4 www records? y/n (lowercase y or n): '))
 
-if ipv6Record:
+if IPV6Record:
     print term.yellow("Conflicting IPV6 @ record detected. Site will not function without updating this record. Update?")
     answerIPV6 = str(raw_input('Update ipv6 @ records? y/n (lowercase y or n): '))
 
-if ipv6wwwRecord:
+if IPV6wwwRecord:
     print term.yellow("Conflicting IPV6 www record detected. Site will not function without updating this record. Update?")
     answerIPV6WWW = str(raw_input('Update ipv6 www records? y/n (lowercase y or n): '))
 
-if answerIPV4 == 'y':
+if answerIPV4.lower() == 'y':
     deleteRecords(zone, CLOUDFLARE_EMAIL, CLOUDFLARE_AUTH_KEY, ipv4DNSRecordExists, ipv4Record)
 
-if answerIPV4WWW == 'y':
+if answerIPV4WWW.lower() == 'y':
     deleteRecords(ipv4wwwDNSRecordExists, zone, ipv4wwwRecord, CLOUDFLARE_EMAIL, CLOUDFLARE_AUTH_KEY, )
 
-if answerIPV6 == 'y':
+if answerIPV6.lower() == 'y':
     deleteRecords(zone, CLOUDFLARE_EMAIL, CLOUDFLARE_AUTH_KEY, ipv6DNSRecordExists, ipv6Record)
 
-if answerIPV6WWW == 'y':
+if answerIPV6WWW.lower() == 'y':
     deleteRecords(ipv6wwwDNSRecordExists,zone, ipv6Record, CLOUDFLARE_EMAIL, CLOUDFLARE_AUTH_KEY, ipv6wwwDNSRecordExists, ipv6wwwRecord)
 # Add Cloudflare records
 print term.blue("Adding records to Cloudflare")
@@ -65,16 +65,13 @@ cf.create_dns_record('www', domain, ipv6, record_type="AAAA")
 print term.green("Cloudflare records added successfully")
 
 
-f = open("./ips.txt", 'w')
-f.write("ipv4="+ipv4 + '\n' + "ipv6="+ipv6)
-f.close()
+with open("./ips.txt", 'w') as f:
+    f.write("ipv4="+ipv4 + '\n' + "ipv6="+ipv6)
 
 print term.blue("Writing ipv4 to Ansible's host file for provisioning.")
 
-f = open("./ansible/hosts", 'w')
-f.write("[VPS]" + '\n'+ ipv4)
-f.close()
-
+with open("./ansible/hosts", 'w') as f:
+    f.write("[VPS]" + '\n'+ ipv4)
 
 print term.magenta("Server created successfully, DNS updated, proceeding to configure the new server as a LEMP host, and downloading and configuring Wordpresss.")
 print term.magenta("Invoking Ansible to configure wordpress server, this may take a while.")
@@ -90,7 +87,7 @@ print term.red("Did server create and provision successfully?")
 successAnswer = str(raw_input('Complete successfully and verified the page loads? y/n (lowercase y/n): '))
 testing = sys.argv[1]
 
-if successAnswer == 'y' and testing != '':
+if successAnswer.lower() == 'y' and testing != '':
     headers = {
     'API-Key': apikey,
     }
