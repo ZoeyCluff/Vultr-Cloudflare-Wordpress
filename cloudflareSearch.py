4e6a1561377ec064c66d6295d53ba15eb7614654
Zoey Cluff <matada89@gmail.com>
2018-01-23 22:05:08 -0600

    add code I didn't commit before starting

--- cloudflareSearch.py
+++ cloudflareSearch.py
@@ -24,7 +24,7 @@ def doRecordsExist(zone, CLOUDFLARE_EMAIL, CLOUDFLARE_AUTH_KEY):
     }
     response = requests.request("GET", url, headers=headers)
     for res in response.json()['result_info']:
-        count = response.json()['result_info']['total_count']   
+        count = response.json()['result_info']['total_count']
         default = 0
         if count  > default:
             # This should be True rather than a string
@@ -32,77 +32,41 @@ def doRecordsExist(zone, CLOUDFLARE_EMAIL, CLOUDFLARE_AUTH_KEY):
             return exists
 
 
-def IPV4Record(zone,  domain, CLOUDFLARE_EMAIL, CLOUDFLARE_AUTH_KEY):
-    if zone:
-        url = "https://api.cloudflare.com/client/v4/zones/" + zone + "/dns_records"
-        querystring = {"type": "A", "name": domain}
-        headers = {
-            'X-Auth-Email': CLOUDFLARE_EMAIL,
-            'X-Auth-Key': CLOUDFLARE_AUTH_KEY,
-            'Cache-Control': "no-cache"
-        }
-        response = requests.request("GET", url, headers=headers, params=querystring)
-        for res in response.json()['result']:
-            if res['name'] == domain:
-                # This should be True rather than a string
-                ipv4DNSRecordExists = 'true'
-                ipv4Record = res['id']
-                return (ipv4DNSRecordExists, ipv4Record)
+def RecordsExist(zone, CLOUDFLARE_EMAIL, CLOUDFLARE_AUTH_KEY, domain, domainLong):
+    url = "https://api.cloudflare.com/client/v4/zones/" + zone + "/dns_records"
+    headers = {
+        'X-Auth-Email': CLOUDFLARE_EMAIL,
+        'X-Auth-Key': CLOUDFLARE_AUTH_KEY,
+        'Cache-Control': "no-cache"
+    }
+    flat = response.text
+    response = requests.request("GET", url, headers=headers)
+    for res in response.json()['result']:
+    dictonary = json.loads(flat)['result']
+    print(dictionary)
+        if res['name'] == domain and res['type'] == 'A':
+              # ipv4Exists = 'True'
+              ipv4Zone = res['']['id'][1]
+              print(ipv4Zone)
+              return ipv4Zone
+    for res in response.json()['result']:
+        if res['name'] == domainLong and res['type'] == 'A':
+             # ipv4wwwExists = 'True'
+             ipv4wwwZone = res['id'][2]
+             print(ipv4wwwZone)
+             return  ipv4wwwZone
+
+    for res in response.json()['result']:
+        if res['name'] == domain and res['type'] == 'AAAA':
+             # ipv6Exists = 'True'
+             ipv6Zone = res['id']
+             print(ipv6Zone)
+             return ipv6Zone
 
 
-# The only difference between this and IPV4Record is that this uses domainLong
-def IPV4wwwRecord(zone,  domainLong, CLOUDFLARE_EMAIL, CLOUDFLARE_AUTH_KEY):
-    if zone:
-        url = "https://api.cloudflare.com/client/v4/zones/" + zone + "/dns_records"
-        querystring = {"type": "A", "name": domainLong}
-        headers = {
-            'X-Auth-Email': CLOUDFLARE_EMAIL,
-            'X-Auth-Key': CLOUDFLARE_AUTH_KEY,
-            'Cache-Control': "no-cache"
-        }
-        response = requests.request(
-            "GET", url, headers=headers, params=querystring)
-        for res in response.json()['result']:
-            if res['name'] == domainLong:
-                # This should be True rather than a string
-                ipv4wwwDNSRecordExists = 'true'
-                ipv4wwwRecord = res['id']
-                return (ipv4wwwDNSRecordExists, ipv4wwwRecord)
-
-
-def IPV6Record(zone,  domain, CLOUDFLARE_EMAIL, CLOUDFLARE_AUTH_KEY):
-    if zone:
-        url = "https://api.cloudflare.com/client/v4/zones/" + zone + "/dns_records"
-        querystring = {"type": "AAAA", "name": domain}
-        headers = {
-            'X-Auth-Email': CLOUDFLARE_EMAIL,
-            'X-Auth-Key': CLOUDFLARE_AUTH_KEY,
-            'Cache-Control': "no-cache"
-        }
-        response = requests.request(
-            "GET", url, headers=headers, params=querystring)
-        for res in response.json()['result']:
-            if res['name'] == domain:
-                # This should be True rather than a string
-                ipv6DNSRecordExists = 'true'
-                ipv6Record = res['id']
-                return (ipv6DNSRecordExists, ipv6Record)
-
-# The only difference between this and IPV6Record is that this uses domainLong
-def IPV6wwwRecord(zone,  domainLong, CLOUDFLARE_EMAIL, CLOUDFLARE_AUTH_KEY):
-    if zone:
-        url = "https://api.cloudflare.com/client/v4/zones/" + zone + "/dns_records"
-        querystring = {"type": "AAAA", "name": domainLong}
-        headers = {
-            'X-Auth-Email': CLOUDFLARE_EMAIL,
-            'X-Auth-Key': CLOUDFLARE_AUTH_KEY,
-            'Cache-Control': "no-cache"
-        }
-        response = requests.request(
-            "GET", url, headers=headers, params=querystring)
-        for res in response.json()['result']:
-            if res['name'] == domainLong:
-                # This should be True rather than a string
-                ipv6wwwDNSRecordExists = 'true'
-                ipv6wwwRecord = res['id']       
-                return (ipv6wwwDNSRecordExists, ipv6wwwRecord)
+    for res in response.json()['result']:
+        if res['name'] == domainLong and res['type'] == 'AAAA':
+             # ipv6wwwExists = 'True'
+             ipv6wwwZone = res['id']
+             print(ipv6wwwZone)
+             return ipv6wwwZone
